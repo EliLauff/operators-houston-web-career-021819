eb94785eaf18755122f8be7f0a5172dd79cb22d0
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-11 18:53:29 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,14 +2,14 @@ def unsafe?(speed)
   if speed > 60 && speed < 40
     return true
   else
-    return FALSE
+    return false
   end
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 && speed < 40 ? TRUE : FALSE
+	speed > 60 && speed < 40 ? true : false
 end
 	
 
